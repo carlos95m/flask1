3dcb71522fce2755022bfbc61a8a33ba6fdc72b6
carlos95m <51838304+carlos95m@users.noreply.github.com>
2023-05-25 11:16:54 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
-from flask import Flask, jsonify
-import os
+from flask import Flask, request
+import rembg
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+@app.route('/remove_background', methods=['POST'])
+def remove_background():
+    # Obtener la imagen enviada por la aplicación Android
+    image_file = request.files['image']
+
+    # Leer los datos de la imagen
+    image_data = image_file.read()
+
+    # Eliminar el fondo de la imagen utilizando rembg
+    output_data = rembg.remove(image_data)
+
+    # Devolver la imagen resultante sin fondo como respuesta
+    return output_data, 200, {'Content-Type': 'image/png'}
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+    app.run(host='0.0.0.0', port=80)
